bd696c416d52056306be585d479db8f3771b02de
VasconcelosCarla <carlajulyanne@outlook.com>
2022-06-12 13:26:00 -0300

    ainda muito ruim

--- sketch.js
+++ sketch.js
@@ -10,8 +10,9 @@ var gameState = WAIT;
 var bg, bgImg;
 var apoio
 var blug, blugImg, bg2, bg2Img;
-var plataforma, plataformaGroup;
+var plataforma, plataformaGroup, plataformaImg, plataformaInvisivel, plataformaInvisivelGroup;
 var buttom, buttomImg;
+
 
 //Função para carregamento de animação e imagens
 function preload(){
@@ -20,11 +21,12 @@ function preload(){
    "./assets/blug4right.png", "./assets/blug3right.png", "./assets/blug2right.png", "./assets/blugright.png");
    bg2Img = loadImage("./assets/bg2.png");
   buttomImg = loadAnimation("./assets/botao1.png", "./assets/botao2.png");
+  plataformaImg = loadImage("./assets/plataforma.png");
   }
 
 
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(800, 750);
   bg = createSprite(400,-1600);
   bg.addAnimation("ceu", bgImg);
   bg.scale = 5;
@@ -33,13 +35,18 @@ function setup() {
   bg2.scale = 4;
   //bg.velocityY = 1;
   plataformaGroup = createGroup();
+  plataformaInvisivelGroup = createGroup();
 
-  blug = createSprite(400, 700);
+  blug = createSprite(400, 500);
   blug.addAnimation("rigth", blugImg);
   
   buttom = createSprite(400, 400);
   buttom.addAnimation("botão", buttomImg)
   buttom.scale = 3;
+
+  blug.setCollider("circle", 0, 0, 20);
+  blug.debug = true;
+  
 
   
    
@@ -61,10 +68,11 @@ function draw() {
   }
   else if(gameState === PLAY){
     buttom.visible = false;
+    bg.velocityY = 1;
+    bg2.velocityY = 1;
     if(keyDown("space")){
-      bg.y = bg.y+6;
-      bg2.y = bg2.y+6;
-      blug.velocityY = -5;
+      
+      blug.velocityY = -10;
     }
 
     blug.velocityY = blug.velocityY + 0.5;
@@ -89,16 +97,31 @@ function draw() {
  
   drawSprites();
   
-  blug.collide(plataformaGroup);
+  blug.collide(plataformaInvisivelGroup);
 }
 function gerarPlataforma(){
   
   if(frameCount % 80 ===0){
-    plataforma = createSprite(200, 100, 120, 20);
+
+    plataforma = createSprite(200, 250, 120, 20);
     plataforma.x = Math.round(random(0, 800));
-    plataforma.velocityY = 3;
-    plataforma.timelife = 200;
-    plataformaGroup.add(plataforma);
+    plataforma.velocityY = 1;
     
+    plataforma.addImage("plataforma", plataformaImg);
+    plataforma.scale = 2;
+    plataformaGroup.add(plataforma);
+
+    plataformaInvisivel = createSprite(200, 250, 120, 10);
+    plataformaInvisivel.x = plataforma.x;
+    plataformaInvisivel.velocityY = 1;
+    
+   
+    plataformaInvisivelGroup.add(plataformaInvisivel);
+    plataformaInvisivel.visible = false;
+
+    plataforma.lifetime  = 150; //tinha um erro de sintaxe aqui, corrigi. é isso que dar programar com sono
+    plataformaInvisivel.lifetime  = 150;
   }
+  
+  
 }
